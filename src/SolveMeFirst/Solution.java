941c71a64eb621286fba8bffe1712a93feea0460
Jake Butler <JakeButler.student@careerdevs.com>
2022-09-22 19:31:38 -0400

    Initial commit

---
+++ src/SolveMeFirst/Solution.java
@@ -0,0 +1,15 @@
+package SolveMeFirst;
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner keyboard = new Scanner(System.in);
+        int a = keyboard.nextInt();
+        int b = keyboard.nextInt();
+        int ans = a + b;
+        System.out.println(ans);
+    }
+}
