2a72784876a5da3690dfc97d3eecf3117fe5199a
Jake Butler <JakeButler.student@careerdevs.com>
2022-12-12 21:48:18 -0500

    Completed three exercises

---
+++ src/compareTheTriplets/Solution.java
@@ -0,0 +1,77 @@
+package compareTheTriplets;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        // Initialize all variables
+        int aPoints = 0;
+        int bPoints = 0;
+        // Create instance of list to be used later
+        List<Integer> eachPoints = new ArrayList<>();
+
+        // Loop through list for a and for b, comparing the elements at each index
+        for (int i = 0; i < a.size(); i++) {
+            // Comparison logic, increment points if one is larger
+            if (a.get(i) > b.get(i)) {
+                aPoints+= 1;
+            } else if (a.get(i) < b.get(i)) {
+                bPoints += 1;
+            }
+
+        }
+        // Add points total to returned List
+        eachPoints.add(aPoints);
+        eachPoints.add(bPoints);
+        // return output
+        return eachPoints;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
